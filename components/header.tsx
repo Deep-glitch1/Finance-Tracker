0acfd069158c78a459da8cc90b0458785dcd4e5f
Deep-glitch1 <176502655+Deep-glitch1@users.noreply.github.com>
2025-03-30 14:49:36 +0530

    Initial commit

---
+++ components/header.tsx
@@ -0,0 +1,20 @@
+import { UserButton } from "@clerk/nextjs"
+import { HeaderLogo } from "./header-logo"
+import { Navigation } from "./navigation"
+import { WelcomeMsg } from "./welcome-Msg"
+export const Header = () =>{
+    return(
+        <header className="bg-gradient-to-b from-green-700 to-green-500 px-4 py-8 lg:px-14 pb-36">
+              <div className="max-w-screen-2xl mx-auto">
+                <div className="w-full flex items-center justify-between mb-14">
+                    <div className="flex items-center lg:gap-x-16">
+                        <HeaderLogo />
+                        <Navigation />
+                    </div>
+                    <UserButton afterSwitchSessionUrl="/" />
+                </div>
+                <WelcomeMsg />
+              </div>
+        </header>
+    )
+}
